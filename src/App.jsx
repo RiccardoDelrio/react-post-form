9b34a1d9b34190efeb56464da846526a354ba906
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-31 12:33:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Let's gooo</h1>
+
+    </>
+  )
+}
+
+export default App
